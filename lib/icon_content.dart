0ed4eb77e7bd3f09950311050b347cc844ed8dc1
Saurav <sp.gharti@gmail.com>
2020-05-12 10:32:53 +0545

    add separate constants file, update height card with slider

--- lib/icon_content.dart
+++ lib/icon_content.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-
-const labelStyle = TextStyle(
-  fontSize: 18.0,
-  color: Color(0xFF8D8E98),
-);
+import 'constants.dart';
 
 class IconContent extends StatelessWidget {
   final IconData icon;
@@ -23,7 +19,7 @@ class IconContent extends StatelessWidget {
         SizedBox(
           height: 15.0,
         ),
-        Text(label, style: labelStyle),
+        Text(label, style: kLabelStyle),
       ],
     );
   }
